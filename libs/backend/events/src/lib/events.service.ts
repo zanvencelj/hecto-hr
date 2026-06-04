a7ee4809ac18665afea3a9df9678be8f86c87c8c
Zan Vencelj <z.vencelj@cargox.io>
2026-06-04 17:03:12 +0200

    fix: Remove unused import

--- libs/backend/events/src/lib/events.service.ts
+++ libs/backend/events/src/lib/events.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, BadRequestException, ForbiddenException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import type { WorkEvent, EventChangeRequest } from '@hecto/database';
 import type {
   WorkEventPublic,
